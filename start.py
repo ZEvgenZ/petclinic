b4548c1c9acd7603a2fe01cef14c49ba8448c72b
Rudya93 <rudyaa93@gmail.com>
2018-05-16 19:26:06 +0300

    Update start.py

--- start.py
+++ start.py
@@ -14,7 +14,7 @@ subnet ='subnet-c8fc75b1'
 instances = ec2.create_instances(
         ImageId='ami-4e79ed36', 
         MinCount=1, 
-        MaxCount=2,
+        MaxCount=1,
         InstanceType="t2.micro",
     KeyName='oRudenk',
     
@@ -22,8 +22,28 @@ instances = ec2.create_instances(
    
 
 for instance in instances:
-    instance.create_tags(Tags=[{"Key": "Name", "Value": "Worker"}])
+    instance.create_tags(Tags=[{"Key": "Name", "Value": "db"}])
     instance.wait_until_running()
 
 for instance in instances:
-    print(instance.id, instance.instance_type, instance.vpc)
+    print(instance.id, instance.instance_type, instance.vpc, instance.private_ip_address, instance.public_ip_address)
+    file = open(“hosts”,”w”) file.write(instance.private_ip_address + "app") file.close() 
+
+###########################################################################
+instances = ec2.create_instances(
+        ImageId='ami-4e79ed36', 
+        MinCount=1, 
+        MaxCount=1,
+        InstanceType="t2.micro",
+    KeyName='oRudenk',
+    
+    NetworkInterfaces=[{'SubnetId': subnet, 'DeviceIndex': 0, 'AssociatePublicIpAddress': True, 'Groups': ['sg-6f437411']}])
+   
+
+for instance in instances:
+    instance.create_tags(Tags=[{"Key": "Name", "Value": "app"}])
+    instance.wait_until_running()
+
+for instance in instances:
+    print(instance.id, instance.instance_type, instance.vpc, instance.private_ip_address, instance.public_ip_address)
+    file = open(“hosts”,”w”) file.write(instance.private_ip_address + "app") file.close() 
